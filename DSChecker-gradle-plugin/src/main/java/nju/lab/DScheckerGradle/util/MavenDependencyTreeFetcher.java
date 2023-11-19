6d0656e9c2d0696e2121e3922376e0132fe5b536
chaicho <1459429549@qq.com>
2023-11-19 16:02:19 +0800

    Parse the maven dependency tree.

--- DSChecker-gradle-plugin/src/main/java/nju/lab/DScheckerGradle/util/MavenDependencyTreeFetcher.java
+++ DSChecker-gradle-plugin/src/main/java/nju/lab/DScheckerGradle/util/MavenDependencyTreeFetcher.java
@@ -1,20 +1,73 @@
 package nju.lab.DScheckerGradle.util;
 
+import org.neo4j.cypher.internal.frontend.v2_3.ast.functions.Has;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MavenDependencyTreeFetcher {
-    public static Map<String, String> getDepVersFromProject(String modulePath, String runtimeClasspath) {
+    private static final Pattern DEPENDENCY_PATTERN = Pattern.compile("([^:]+):([^:]+):[^:]+:([^:]+):[^:]+");
 
+    public static Map<String, String> getDepVersFromProject(String modulePath) {
+        String dependencyTree = runDependenciesPlugin(modulePath);
+        if (dependencyTree != null) {
+            return parseDependencyTree(dependencyTree);
+        }
         return null;
+    }
+    // A method that takes a dependency tree as a string and returns a map of GA to version
+    public static Map<String, String> parseDependencyTree(String dependencyTree) {
+        // Create an empty map to store the result
+        Map<String, String> map = new HashMap<>();
+
+        // Split the dependency tree by line separator
+        String[] lines = dependencyTree.split(System.lineSeparator());
+        // Loop through each line
+        for (String line : lines) {
+            // Remove the [INFO] prefix and any leading or trailing whitespace
+            line = line.replace("[INFO]", "").trim();
+            if (line.split(" ").length <= 1){
+                continue;
+            }
+            line = line.split(" ")[1];
+            // Skip empty lines
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            // Remove the +- symbols from the line
+//            line = line.replaceAll("[+-|]", "");
+//            line = line.trim();
+            // Match the line with the dependency pattern
+            Matcher matcher = DEPENDENCY_PATTERN.matcher(line);
+
+            // If the line matches the pattern, extract the GA and version and put them in the map
+            if (matcher.find()) {
+                String groupId = matcher.group(1);
+                String artifactId = matcher.group(2);
+                String version = matcher.group(3);
+                String ga = groupId + ":" + artifactId;
+                map.put(ga, version);
+            }
+        }
+
+        // Return the map
+        return map;
     }
 
     public static void main(String[] args) {
-        String projectPath = "/root/dependencySmell/evaluation/happenedSmells/projectsDir/msgraph-sdk-java-core-6110bcfa7bfdc21b20e7244a48c01f1bb8a78569"; // Replace with your project path
-        runDependenciesPlugin(projectPath);
+        String projectPath = "/home/chaicho/PathToDoc/DependencySmell/Detection/DepAna"; // Replace with your project path
+        HashMap<String, String> depVers = (HashMap<String, String>) getDepVersFromProject(projectPath);
+        for (String dep : depVers.keySet()) {
+            System.out.println(dep);
+            System.out.println(depVers.get(dep));
+        }
     }
 
     private static String runDependenciesPlugin(String projectPath) {
